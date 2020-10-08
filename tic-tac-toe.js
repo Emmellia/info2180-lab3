77f6f5f1146fe061dca2960753c42458e1b9b0a7
Emmellia Hayden <emmelliahayden@gmail.com>
2020-10-07 23:01:39 -0400

    Added initial code

---
+++ tic-tac-toe.js
@@ -0,0 +1,9 @@
+"use strict";
+
+    window.onload= function(){
+    var body= document.getElementsByTagName("body");
+    var element=document.getElementByTagId("board").childNodes.className="square";
+
+    
+    };
+
